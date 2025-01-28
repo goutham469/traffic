e5fb24aece78d220e1ba02a51e82b59cd2dd3e7d
goutham <uppinurigouthamreddy@gmail.com>
2025-01-28 21:30:07 +0530

    for admin api changed2

--- APIs/adminAPI.js
+++ APIs/adminAPI.js
@@ -36,6 +36,23 @@ adminAPI.post('/update-to-basic-account' , async (req,res)=>{
     }
 })
 
+adminAPI.get('/all-sites-names' , async(req,res)=>{
+    let data = await req.sitesCollection.find( {} , { stats:0,ips:0,countrys:0,logs:0,referrers:0,devices:0,browsers:0 }).toArray()
+    res.send({success:true,data:data})
+})
+
+adminAPI.get('/sessions' , async(req,res) => {
+    let data = await req.sessionsCollection.find().toArray()
+    if(data.length > 0)
+    {
+        res.send({status:true , data:data})
+    }
+    else
+    {
+        res.send({status:false , message:"site was not registered for sessions !"})
+    }
+})
+
 
 
 module.exports = adminAPI
